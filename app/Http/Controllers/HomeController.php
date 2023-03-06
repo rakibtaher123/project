52d9e2670dff44acf359cfd662dd8fc4ff4e4c0d
SabbirHR <sabbirhossen860@gmail.com>
2023-03-06 19:31:17 +0600

    update: frontend category, product display ,header

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,27 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Color;
+use App\Models\Size;
+use App\Models\Unit;
+use App\Models\Brand;
+use App\Models\Product;
+use App\Models\Category;
+use App\Models\SubCategory;
 use Facade\FlareClient\View;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-        return View('frontend.welcome');
+    public function index()
+    {
+        $product = Product::where('status', 1)->latest()->limit(12)->get();
+        $categories = Category::all();
+        $subcategories = SubCategory::all();
+        $brands = Brand::all();
+        $units = Unit::all();
+        $sizes = Size::all();
+        $colors = Color::all();
+        return View('frontend.welcome', compact('product', 'categories', 'subcategories', 'brands', 'units', 'sizes', 'colors'));
     }
 }
--- resources/views/frontend/header.blade.php
+++ resources/views/frontend/header.blade.php
@@ -5,10 +5,10 @@
             <ul class="header-links pull-left">
                 <li><a href="#"><i class="fa fa-phone"></i> +021-95-51-84</a></li>
                 <li><a href="#"><i class="fa fa-envelope-o"></i> email@email.com</a></li>
-                <li><a href="#"><i class="fa fa-map-marker"></i> 1734 Stonecoal Road</a></li>
+                <li><a href="#"><i class="fa fa-map-marker"></i> Uttara , Dhaka 1230</a></li>
             </ul>
             <ul class="header-links pull-right">
-                <li><a href="#"><i class="fa fa-dollar"></i> USD</a></li>
+                <li><a href="#"><i class="fa fa-try"></i> BD</a></li>
                 <li><a href="#"><i class="fa fa-user-o"></i> My Account</a></li>
             </ul>
         </div>
@@ -24,8 +24,8 @@
                 <!-- LOGO -->
                 <div class="col-md-3">
                     <div class="header-logo">
-                        <a href="#" class="logo">
-                            <img src="./img/logo.png" alt="">
+                        <a href="{{ url('/') }}" class="logo">
+                            <img src="./img/logo.png" alt="" width="236" height="74px">
                         </a>
                     </div>
                 </div>
@@ -37,8 +37,9 @@
                         <form>
                             <select class="input-select">
                                 <option value="0">All Categories</option>
-                                <option value="1">Category 01</option>
-                                <option value="1">Category 02</option>
+                                @foreach ($categories as $item)
+                                    <option>{{ $item->name }}</option>
+                                @endforeach
                             </select>
                             <input class="input" placeholder="Search here">
                             <button class="search-btn">Search</button>
@@ -97,7 +98,7 @@
                                 </div>
                                 <div class="cart-btns">
                                     <a href="#">View Cart</a>
-                                    <a href="#">Checkout  <i class="fa fa-arrow-circle-right"></i></a>
+                                    <a href="#">Checkout <i class="fa fa-arrow-circle-right"></i></a>
                                 </div>
                             </div>
                         </div>
